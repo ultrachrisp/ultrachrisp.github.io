8c076c2ed08335cccaaa763a2aa8e147670df3e3
Chrisp <ultrachrisp@gmail.com>
2016-03-28 13:55:11 +0200

    rebuilding site Mon Mar 28 13:55:11 SAST 2016

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 
 document.addEventListener('DOMContentLoaded', function() {
   'use strict';
-  //console.log('Wheeeee');
 });
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
